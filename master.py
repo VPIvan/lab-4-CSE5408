7b3863cc72c73a6ed9fd74670d4428c8a876878d
Martin-Coll <99680384+Martin-Coll@users.noreply.github.com>
2022-02-28 12:33:25 -0800

    Martin Added the check if prime code to the master.py file

--- master.py
+++ master.py
@@ -1,3 +1,23 @@
+#String to reverse
 string_to_reverse = input("Enter string: ")
 reversed_string = ''.join(reversed(string_to_reverse))
 print(reversed_string)
+
+#check if prime code
+check_number = int(input("Enter a number to see if it is prime: "))
+# If given number is greater than 1
+if check_number > 1:
+
+    # Iterate from 2 to n / 2
+    for i in range(2, int(check_number/2)+1):
+
+        # If num is divisible by any number between
+        # 2 and n / 2, it is not prime
+        if (check_number % i) == 0:
+            print(check_number, "is not a prime number")
+            break
+    else:
+        print(check_number, "is a prime number")
+
+else:
+    print(check_number, "is not a prime number")
