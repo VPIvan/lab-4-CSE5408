eccff73d7426443f5d257882b83a016eca17611a
Martin-Coll <99680384+Martin-Coll@users.noreply.github.com>
2022-02-28 12:28:01 -0800

    This is the check if a number is prime file

---
+++ check_if_prime.py
@@ -0,0 +1,17 @@
+check_number = int(input("Enter a number to see if it is prime: "))
+# If given number is greater than 1
+if check_number > 1:
+
+    # Iterate from 2 to n / 2
+    for i in range(2, int(check_number/2)+1):
+
+        # If num is divisible by any number between
+        # 2 and n / 2, it is not prime
+        if (check_number % i) == 0:
+            print(check_number, "is not a prime number")
+            break
+    else:
+        print(check_number, "is a prime number")
+
+else:
+    print(check_number, "is not a prime number")
